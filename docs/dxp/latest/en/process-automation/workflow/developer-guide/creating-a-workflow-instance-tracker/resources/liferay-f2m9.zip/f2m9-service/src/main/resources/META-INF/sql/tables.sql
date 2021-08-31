24af56048438d3cb6a1f67f015d3224085dfa451
Russell Bohl <russell.bohl@liferay.com>
2021-08-31 15:53:40 -0400

    LRDOCS-9702 initial code with a simple Todo list Service Builder Application

---
+++ docs/dxp/latest/en/process-automation/workflow/developer-guide/creating-a-workflow-instance-tracker/resources/liferay-f2m9.zip/f2m9-service/src/main/resources/META-INF/sql/tables.sql
@@ -0,0 +1,11 @@
+create table F2M9_Todo (
+	uuid_ VARCHAR(75) null,
+	todoId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	name VARCHAR(75) null
+);
---
+++ docs/dxp/latest/en/process-automation/workflow/developer-guide/creating-a-workflow-instance-tracker/resources/liferay-f2m9.zip/f2m9-web/src/main/java/com/acme/f2m9/web/internal/portlet/F2M9Portlet.java
@@ -0,0 +1,64 @@
+package com.acme.f2m9.web.internal.portlet;
+
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.acme.f2m9.model.Todo;
+import com.acme.f2m9.service.TodoLocalService;
+import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"javax.portlet.display-name=F2M9 Portlet",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language"
+	},
+	service = Portlet.class
+)
+public class F2M9Portlet extends MVCPortlet {
+
+	public void addTodo(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String item = ParamUtil.getString(actionRequest, "item");
+
+		User user = _portal.getUser(actionRequest);
+
+		Todo todo = _todoLocalService.createTodo(
+			CounterLocalServiceUtil.increment());
+
+		todo.setCompanyId(user.getCompanyId());
+		todo.setUserId(user.getUserId());
+		todo.setUserName(user.getFullName());
+
+		todo.setGroupId(themeDisplay.getSiteGroupId());
+
+		todo.setName(item);
+
+		_todoLocalService.addTodo(todo);
+	}
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private TodoLocalService _todoLocalService;
+
+}
